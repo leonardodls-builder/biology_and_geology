630a35481a7f65ba58f7bae61e691ec2b5ba8c9f
aditi-kapur <>
2020-04-21 18:23:04 +0530

    Updated version of order list player

--- assets/public/LO-2/data/consumerConfig.js
+++ assets/public/LO-2/data/consumerConfig.js
@@ -50,8 +50,8 @@ var consumerConfig = {
       },
       "order-list-player": {
         "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/order-list-player/releases/",
-        "version": "0.0.3",
-        "js": "0.0.3/libs-order-list-item-player",
+        "version": "0.0.4",
+        "js": "0.0.4/libs-order-list-item-player",
         "dependencies": [
           "react",
           "react-dom",
