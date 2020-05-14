c8da7e459d4fc10277dcca344dfb8802cb791d88
aditi-kapur <>
2020-05-14 13:54:23 +0530

    updating versions of item players

--- assets/public/LO-2/data/consumerConfig.js
+++ assets/public/LO-2/data/consumerConfig.js
@@ -9,8 +9,8 @@ var consumerConfig = {
       },
       "imagelabel-dnd": {
         "baseURL": "https://sdk-qa.leonardodls.com/modules/image-label-dnd-player/releases/",
-        "version": "0.0.31",
-        "js": "0.0.31/libs-imagelabel-dnd-item-player",
+        "version": "0.1.0",
+        "js": "0.1.0/libs-imagelabel-dnd-item-player",
         "dependencies": [
           "react",
           "react-dom",
@@ -21,8 +21,8 @@ var consumerConfig = {
       },
       "mcsr-block": {
         "baseURL": "https://sdk-qa.leonardodls.com/modules/mcq-block-player/releases/",
-        "version": "0.0.27",
-        "js": "0.0.27/libs-mcsr-item-player",
+        "version": "0.1.0",
+        "js": "0.1.0/libs-mcsr-item-player",
         "dependencies": [
           "react",
           "react-dom",
@@ -31,8 +31,8 @@ var consumerConfig = {
       },
       "fib-dnd": {
         "baseURL": "https://sdk-qa.leonardodls.com/modules/fib-dnd-player/releases/",
-        "version": "0.0.20",
-        "js": "0.0.20/libs-fib-dnd-item-player",
+        "version": "0.1.0",
+        "js": "0.1.0/libs-fib-dnd-item-player",
         "dependencies": [
           "react",
           "react-dom",
@@ -43,8 +43,8 @@ var consumerConfig = {
       },
       "order-list": {
         "baseURL": "https://sdk-qa.leonardodls.com/modules/order-list-player/releases/",
-        "version": "0.0.10",
-        "js": "0.0.10/libs-order-list-item-player",
+        "version": "0.1.0",
+        "js": "0.1.0/libs-order-list-item-player",
         "dependencies": [
           "react",
           "react-dom",
@@ -62,17 +62,17 @@ var consumerConfig = {
         "js": "16.12.0/react-dom.development"
       },
       "react-dnd": {
-        "baseURL": "https://cdn.jsdelivr.net/npm/react-dnd@10.0.2/dist/umd/",
-        "js": "ReactDnD.min"
+        "baseURL": "https://sdk-qa.leonardodls.com/modules/react-dnd/releases/",
+        "js": "10.0.2/ReactDnD.min"
       },
       "react-dnd-html5-backend": {
-        "baseURL": "https://cdn.jsdelivr.net/npm/react-dnd-html5-backend@10.0.2/dist/umd/",
-        "js": "ReactDnDHTML5Backend.min"
+        "baseURL": "https://sdk-qa.leonardodls.com/modules/react-dnd-html5-backend/releases/",
+        "js": "10.0.2/ReactDnDHTML5Backend.min"
       },
       "libs-player-ui-components": {
         "baseURL": "https://sdk-qa.leonardodls.com/modules/libs-player-ui-components/releases/",
-        "version": "0.0.20",
-        "js": "0.0.20/libs-player-ui-components"
+        "version": "0.1.0",
+        "js": "0.1.0/libs-player-ui-components"
       }
     }
   }
