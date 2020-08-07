5303420ddfa84d54bc35361e7ce8fe44e8baffd8
jasdeep <jasdeep.singh@comprotechnologies.com>
2020-08-07 09:51:43 +0530

    migrating from stage to prod

--- assets/public/LO-2/data/consumerConfig.js
+++ assets/public/LO-2/data/consumerConfig.js
@@ -2,13 +2,13 @@ var consumerConfig = {
   "renderers": {
     "modules": {
       "itembank-player": {
-        "baseURL": "https://sdk-stg.leonardodls.com/modules/multi-item-activity-player/releases/",
+        "baseURL": "https://sm.sdk.leonardodls.com/modules/multi-item-activity-player/releases/",
         "version": "0.0.33",
         "js": "0.0.33/multiItemActivityPlayer",
         "css": "0.0.33/multiItemActivityPlayer"
       },
       "imagelabel-dnd": {
-        "baseURL": "https://sdk-stg.leonardodls.com/modules/image-label-dnd-player/releases/",
+        "baseURL": "https://sm.sdk.leonardodls.com/modules/image-label-dnd-player/releases/",
         "version": "0.20.0",
         "js": "0.20.0/libs-imagelabel-dnd-item-player",
         "dependencies": [
@@ -20,7 +20,7 @@ var consumerConfig = {
         ]
       },
       "mcsr-block": {
-        "baseURL": "https://sdk-stg.leonardodls.com/modules/mcq-block-player/releases/",
+        "baseURL": "https://sm.sdk.leonardodls.com/modules/mcq-block-player/releases/",
         "version": "0.13.0",
         "js": "0.13.0/libs-mcsr-item-player",
         "dependencies": [
@@ -30,7 +30,7 @@ var consumerConfig = {
         ]
       },
       "fib-dnd": {
-        "baseURL": "https://sdk-stg.leonardodls.com/modules/fib-dnd-player/releases/",
+        "baseURL": "https://sm.sdk.leonardodls.com/modules/fib-dnd-player/releases/",
         "version": "0.18.0",
         "js": "0.18.0/libs-fib-dnd-item-player",
         "dependencies": [
@@ -42,7 +42,7 @@ var consumerConfig = {
         ]
       },
       "order-list": {
-        "baseURL": "https://sdk-stg.leonardodls.com/modules/order-list-player/releases/",
+        "baseURL": "https://sm.sdk.leonardodls.com/modules/order-list-player/releases/",
         "version": "0.14.0",
         "js": "0.14.0/libs-order-list-item-player",
         "dependencies": [
@@ -52,25 +52,25 @@ var consumerConfig = {
         ]
       },
       "react": {
-        "baseURL": "https://sdk-stg.leonardodls.com/modules/react/releases/",
+        "baseURL": "https://sm.sdk.leonardodls.com/modules/react/releases/",
         "version": "16.12.0",
         "js": "16.12.0/react.production.min"
       },
       "react-dom": {
-        "baseURL": "https://sdk-stg.leonardodls.com/modules/react-dom/releases/",
+        "baseURL": "https://sm.sdk.leonardodls.com/modules/react-dom/releases/",
         "version": "16.12.0",
         "js": "16.12.0/react-dom.production.min"
       },
       "react-dnd": {
-        "baseURL": "https://sdk-stg.leonardodls.com/modules/react-dnd/releases/",
+        "baseURL": "https://sm.sdk.leonardodls.com/modules/react-dnd/releases/",
         "js": "10.0.2/ReactDnD.min"
       },
       "react-dnd-html5-backend": {
-        "baseURL": "https://sdk-stg.leonardodls.com/modules/react-dnd-html5-backend/releases/",
+        "baseURL": "https://sm.sdk.leonardodls.com/modules/react-dnd-html5-backend/releases/",
         "js": "10.0.2/ReactDnDHTML5Backend.min"
       },
       "libs-player-ui-components": {
-        "baseURL": "https://sdk-stg.leonardodls.com/modules/libs-player-ui-components/releases/",
+        "baseURL": "https://sm.sdk.leonardodls.com/modules/libs-player-ui-components/releases/",
         "version": "0.16.0",
         "js": "0.16.0/libs-player-ui-components"
       }
